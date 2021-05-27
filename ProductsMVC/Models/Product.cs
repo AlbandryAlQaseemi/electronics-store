269ff9231bfc27c0613301502b5cad7fc15252de
Albandry AlQaseemi <Albandr.alqaseemi@gmail.com>
2021-05-27 04:02:55 +0300

    commit files

---
+++ ProductsMVC/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductsMVC.Models
+{
+    public class Product
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public decimal price { get; set; }
+        public string model { get; set; }
+
+        public string image { get; set; }
+
+        internal static Product Find(Func<object, bool> p)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
